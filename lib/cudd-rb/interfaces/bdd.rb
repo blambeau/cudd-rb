a94a6553acb2830fb064e02e5ee97715b37b8dc0
Bernard Lambeau <blambeau@gmail.com>
2012-07-18 11:14:02 +0200

    Reorganise Interface::BDD code and add #satisfiable?

--- lib/cudd-rb/interfaces/bdd.rb
+++ lib/cudd-rb/interfaces/bdd.rb
@@ -2,70 +2,7 @@ module Cudd
   module Interface
     module BDD
 
-      # Returns the number of BDD variables in use
-      #
-      # @see Cudd_ReadSize
-      def size
-        Wrapper.ReadSize(native_manager)
-      end
-      alias :bdd_count :size
-
-      # Returns the variable index of a given node.
-      #
-      # @see Cudd_NodeReadIndex
-      def var_index(bdd)
-        Wrapper.NodeReadIndex(bdd)
-      end
-
-      # Returns the bdd ONE
-      #
-      # @see Cudd_ReadOne
-      def one
-        _bdd Wrapper.ReadOne(native_manager)
-      end
-
-      # Returns the bdd ZERO
-      #
-      # @see Cudd_ReadLogicZero
-      def zero
-        _bdd Wrapper.ReadLogicZero(native_manager)
-      end
-
-      # Returns the BDD variable with index i.
-      #
-      # @param [Integer] i a variable index
-      # @see Cudd_bddIthVar
-      def ith_var(i)
-        _bdd Wrapper.bddIthVar(native_manager, i)
-      end
-
-      # Returns a new BDD variable.
-      #
-      # @see Cudd_bddNewVar
-      def new_var
-        _bdd Wrapper.bddNewVar(native_manager)
-      end
-
-      # Returns the negation of a BDD.
-      #
-      # @see Cudd_bddNot
-      def not(f)
-        _bdd Wrapper.bddNot(native_manager, f)
-      end
-
-      # Returns the conjunction of two BDDs.
-      #
-      # @see Cudd_bddAnd
-      def and(f, g)
-        _bdd Wrapper.bddAnd(native_manager, f, g)
-      end
-
-      # Returns the disjunction of two BDDs.
-      #
-      # @see Cudd_bddOr
-      def or(f, g)
-        _bdd Wrapper.bddOr(native_manager, f, g)
-      end
+      ### REFERENCE COUNT ################################################################
 
       # Increases the reference count of `f` and returns it.
       #
@@ -91,14 +28,92 @@ module Cudd
         f
       end
 
+      ### VARS ###########################################################################
+
+      # Returns the number of BDD variables in use
+      #
+      # @see Cudd_ReadSize
+      def size
+        Wrapper.ReadSize(native_manager)
+      end
+      alias :bdd_count :size
+
+      # Returns the variable index of a given node.
+      #
+      # @see Cudd_NodeReadIndex
+      def var_index(bdd)
+        Wrapper.NodeReadIndex(bdd)
+      end
+
+      # Returns the BDD variable with index i.
+      #
+      # @param [Integer] i a variable index
+      # @see Cudd_bddIthVar
+      def ith_var(i)
+        _bdd Wrapper.bddIthVar(native_manager, i)
+      end
+
+      # Returns a new BDD variable.
+      #
+      # @see Cudd_bddNewVar
+      def new_var
+        _bdd Wrapper.bddNewVar(native_manager)
+      end
+
+      ### CONSTANTS ######################################################################
+
+      # Returns the bdd ONE
+      #
+      # @see Cudd_ReadOne
+      def one
+        _bdd Wrapper.ReadOne(native_manager)
+      end
+
+      # Returns the bdd ZERO
+      #
+      # @see Cudd_ReadLogicZero
+      def zero
+        _bdd Wrapper.ReadLogicZero(native_manager)
+      end
+
+      ### BOOLEAN ALGEBRA ################################################################
+
+      # Returns the negation of a BDD.
+      #
+      # @see Cudd_bddNot
+      def not(f)
+        _bdd Wrapper.bddNot(native_manager, f)
+      end
+
+      # Returns the conjunction of two BDDs.
+      #
+      # @see Cudd_bddAnd
+      def and(f, g)
+        _bdd Wrapper.bddAnd(native_manager, f, g)
+      end
+
+      # Returns the disjunction of two BDDs.
+      #
+      # @see Cudd_bddOr
+      def or(f, g)
+        _bdd Wrapper.bddOr(native_manager, f, g)
+      end
+
+      ### EVALUATION & SATISFIABILITY ####################################################
+
       # Returns the value of a BDD for a given variable assignment `input`.
       #
       # @see Cudd_Eval
-      def eval(f, input)
-        assignment = Assignment.new(self, input)
+      def eval(f, assignment)
+        assignment = Assignment.new(self, assignment)
         assignment.with_memory_pointer do |ptr|
           _bdd Wrapper.Eval(native_manager, f, ptr)
         end
+      end
+
+      # Returns true if `bdd` is satisfiable, false otherwise.
+      def satisfiable?(bdd)
+        !bdd.zero?
       end
 
     private
